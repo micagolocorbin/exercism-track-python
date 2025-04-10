f9ec8f448527e67265b2ef699e910a10b5c12b62
micagolocorbin <micagolocorbin@gmail.com>
2025-04-10 19:53:27 +0200

    solution for 7th exercise

--- little-sisters-essay/string_methods.py
+++ little-sisters-essay/string_methods.py
@@ -25,7 +25,8 @@ def clean_up_spacing(sentence):
     """Verify that there isn't any whitespace at the start and end of the sentence.
 
     :param sentence: str - a sentence to clean of leading and trailing space characters.
-    :return: str - a sentence that has been cleaned of leading and trailing space characters.
+    :return: str - a sentence that has been cleaned of leading and trailing space
+    characters.
     """
 
     return sentence.strip()
